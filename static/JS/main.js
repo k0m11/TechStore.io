2441a9f1b051102cb19b93fc792230ba96b1b5c2
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-02-27 20:47:09 +0300

    Update main.js

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,7 +1,9 @@
 let shop = document.querySelector("#shop")
 
+let basket = JSON.parse(localStorage.getItem("data")) || []
+
 let generateShop = () => {
-    return (shop.innerHTML = AllDataCarts.map((x)=>{
+    return (shop.innerHTML = HomePageCarts.map((x)=>{
         let {id, name, price, img} = x
         return `
         <div class="card" width="200" id="${id}">
@@ -11,10 +13,8 @@ let generateShop = () => {
         <a href="#"><img width="250" src=${img}  alt=""></a>
         <div class="info-bottom-iMac">
           <p>${price}</p>
-          <div class="buy">
-            <a href="#" onclick="Progress()">
+          <div class="buy" id="${id}" onclick="increment(id)">
               <p>BUY</p>
-            </a>
           </div>
         </div>
       </div>
@@ -23,3 +23,37 @@ let generateShop = () => {
 }
 
 generateShop()
+
+let increment = (id) => {
+  let selected = id
+  let search = basket.find((x)=> x.id === selected.id)
+
+  if(search === undefined){
+    basket.push({
+        id: selected.id,
+        item: 1
+    })
+  }
+  else {
+    search.item += 1
+  }
+  
+  update(selected.id)
+
+  localStorage.setItem("data", JSON.stringify(basket))
+}
+
+let update = (id) => {
+    let search = basket.find((x)=> x.id === id)
+    // Определенный ID Который нужно изменять
+    document.querySelector("#AllProd").innerHTML = search.item
+    calculation()
+}
+
+let calculation = ()=> {
+  let cartIcon = document.querySelector("#AllProd")
+  
+  cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0)
+}
+
+calculation()
