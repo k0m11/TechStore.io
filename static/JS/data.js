0b5b46c474e362e526e029e6a501f45a2ce81d03
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-02-27 21:44:12 +0300

    Update data.js

--- static/JS/data.js
+++ static/JS/data.js
@@ -1,4 +1,4 @@
-let AllDataCarts = [{
+let HomePageCarts = [{
     id: "1j3hjpoqshf",
     img: "https://cdn1.ozone.ru/s3/multimedia-x/wc1000/6423777345.jpg",
     name: "Apple iMac",
@@ -18,13 +18,38 @@ let AllDataCarts = [{
     img: "https://cdn1.ozone.ru/s3/multimedia-7/wc1000/6388813327.jpg",
     name: "AMD Ryzen Threadripper 3970X",
     price: 3290 + "$"
+}]
+
+
+
+
+let AllDataCarts = [{
+    id: "htsdfkghjkl;sd",
+    img: "https://cdn1.ozone.ru/s3/multimedia-x/wc1000/6423777345.jpg",
+    name: "Apple iMac",
+    price: 4090 + "$"
 },{
-    id: "jfaj;sfjkkkl",
+    id: "kqt;alusdgfkl;",
+    img: "https://cdn1.ozone.ru/s3/multimedia-j/wc1000/6416967355.jpg",
+    name: "Apple iPhone 14 Pro Max",
+    price: 2090 + "$"
+},{
+    id: "8iquw;eurtasd",
+    img: "https://cdn1.ozone.ru/s3/multimedia-0/wc1000/6479498256.jpg",
+    name: "Apple MacBook Pro",
+    price: 2800 + "$"
+},{
+    id: "asdjkufhyglasdf",
+    img: "https://cdn1.ozone.ru/s3/multimedia-7/wc1000/6388813327.jpg",
+    name: "AMD Ryzen Threadripper 3970X",
+    price: 3290 + "$"
+},{
+    id: "qowieurq[wer",
     img: "https://cdn1.ozone.ru/s3/multimedia-7/wc1000/6429398467.jpg",
     name: "NVIDIA GeForce RTX 4090",
     price: 6490 + "$"
 },{
-    id: "uiopoputasfj",
+    id: "u[qwr[oqw][roopoqwr",
     img: "https://cdn1.ozone.ru/s3/multimedia-o/wc1000/6249138456.jpg",
     name: "HTC Vive Pro",
     price: 1890 + "$"
