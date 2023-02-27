884a72b13f894cc9edb08688dd34cda8ebce5888
k0m1 <115169534+k0m11@users.noreply.github.com>
2023-02-27 20:46:51 +0300

    Update AllProducts.js

--- static/JS/AllProducts.js
+++ static/JS/AllProducts.js
@@ -1,10 +1,12 @@
 let products = document.querySelector("#prod")
 
+let basket = JSON.parse(localStorage.getItem("data")) || []
+
 let generateShop = () => {
     return(products.innerHTML = AllDataCarts.map((x)=>{
         let {id,name,price,img} = x
         return `
-        <div class="ProdCart">
+        <div class="ProdCart" id="${id}">
         <img src="${img}" width="230px" alt="#">
         <div class="about">
         <div class="Title">
@@ -13,10 +15,8 @@ let generateShop = () => {
         <div class="prices">
             <p>${price}</p>
         </div>
-        <div class="buy">
-        <a href="#" onclick="Progress()">
+        <div class="buy" onclick="increment(id)">
           <p>BUY</p>
-        </a>
         </div>
         </div>
         </div> 
@@ -25,3 +25,38 @@ let generateShop = () => {
 }
 
 generateShop()
+
+let increment = (id) => {
+  let selected = id
+
+  let search = basket.find((x)=> id === selected.id)
+
+  if (search === undefined){
+    basket.push({
+      item: 1,
+      id: selected.id
+    })
+  }
+  else {
+    search.item += 1
+  }
+
+  update(selected.id)
+
+  localStorage.setItem("data", JSON.stringify(basket))
+}
+
+let update = (id) => {
+  let search = basket.find((x)=> x.id === id)
+
+  document.querySelector("#AllProd").innerHTML = search.item
+  calculation()
+}
+
+let calculation = () => {
+  let cartIcon = document.querySelector("#AllProd")
+
+  cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y) => x+y,0)
+}
+
+calculation()
